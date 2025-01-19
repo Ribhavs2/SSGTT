4a8fcbfa12022b18f4bbafb525a02584a77677d0
Kaiwen-UIUC <kaiwen4@illinois.edu>
2025-01-19 02:54:21 -0600

    Changed reasoning LLM and optimized generate_paraphrases function

--- llama3_download.py
+++ llama3_download.py
@@ -1,9 +1,12 @@
-from huggingface_hub import login
+from dotenv import load_dotenv
 import os
-HF_TOKEN = os.getenv("HF_TOKEN")
-
 from huggingface_hub import snapshot_download
 
-local_model_path = "./models/meta-llama-3"
-snapshot_download(repo_id="meta-llama/Meta-Llama-3-8B-Instruct", local_dir=local_model_path)
+load_dotenv()
+HF_TOKEN = os.getenv("HF_TOKEN")
 
+from huggingface_hub import login
+login(token=HF_TOKEN)
+
+local_model_path = "./models/Llama-3.1-8B-Instruct"
+snapshot_download(repo_id="meta-llama/Llama-3.1-8B-Instruct", local_dir=local_model_path)
